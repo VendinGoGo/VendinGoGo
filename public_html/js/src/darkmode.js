3ab6e61a8c18392a1745a7350d559054d3c711af
Kaleb-Pace <kapflight@gmail.com>
2016-04-18 04:01:21 -0500

    Added Darkmode Feature -Added file: js/src/darkmode.js -Added file: style/index-darkmode.css

---
+++ public_html/js/src/darkmode.js
@@ -0,0 +1,27 @@
+$('#mode').change(function() {
+    if($(this).prop('checked'))
+    {
+        $('body').addClass('dark-mode');
+        $('nav').addClass('dark-mode');
+        $('button').addClass('dark-mode');
+        $('.navbar-brand').addClass('dark-mode');
+        $('.dropdown-toggle').addClass('dark-mode');
+        $('.label').addClass('dark-mode');
+        $('.label-success').addClass('dark-mode');
+        $('#profilePic').addClass('dark-mode');
+        $('#brandIcon').addClass('dark-mode');
+    }
+    else
+    {
+        $('body').removeClass('dark-mode');
+        $('nav').removeClass('dark-mode');
+        $('button').removeClass('dark-mode');
+        $('.navbar-brand').removeClass('dark-mode');
+        $('.dropdown-toggle').removeClass('dark-mode');
+        $('.label').removeClass('dark-mode');
+        $('.label-success').removeClass('dark-mode');
+        $('#profilePic').removeClass('dark-mode');
+        $('#brandIcon').removeClass('dark-mode');
+    }
+
+});
