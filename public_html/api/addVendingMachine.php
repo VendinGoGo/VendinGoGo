e1fb3c56da517632ba85136e7579f7ace9db067e
Eli Davis <eli.davis1995@gmail.com>
2016-04-19 13:03:07 -0500

    Bounding number of machines
    Before you could set whatever number you'd like to be for number of
    vending machines while creating a location.  This bounds the number
    between 1 and 9.

--- public_html/api/addVendingMachine.php
+++ public_html/api/addVendingMachine.php
@@ -19,7 +19,7 @@ $stmt = $conn->prepare("INSERT INTO `vendinglocation` (`lat`, `lng`, `submittedB
 
 $lat = filter_input(INPUT_GET, "lat", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 $lng = filter_input(INPUT_GET, "lng", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-$numOfMachines = filter_input(INPUT_GET, "m", FILTER_SANITIZE_NUMBER_INT);
+$numOfMachines = min(max(filter_input(INPUT_GET, "m", FILTER_SANITIZE_NUMBER_INT), 1), 9);
 $howToFind = filter_input(INPUT_GET, "w", FILTER_SANITIZE_STRING);
 
 $stmt->bind_param("ddiis", $lat, $lng, $_SESSION['access_token']['user_id'], $numOfMachines, $howToFind);
